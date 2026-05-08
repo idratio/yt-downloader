cc8c5d46accb5f44b6791c30d9005a323236454e
idratio <thabokuwani@yahoo.com>
2026-05-08 14:02:03 +0200

    Add video download endpoint

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import os
+
 from fastapi import FastAPI
 from yt_dlp import YoutubeDL
+import os
+DOWNLOAD_DIR = "downloads"
+os.makedirs(DOWNLOAD_DIR, exist_ok=True)
 
 app = FastAPI()
 
@@ -18,8 +23,37 @@ def get_info(url: str):
     with YoutubeDL(options) as ydl:
         info = ydl.extract_info(url, download=False)
 
-        return {
-            "title": info["title"],
-            "channel": info["channel"],
-            "duration": info["duration"],
-        }
+    formats = []
+
+    for f in info.get("formats", []):
+
+        formats.append({
+            "format_id": f["format_id"],
+            "ext": f.get("get"),
+            "resolution": f.get("resolution")
+        })
+
+    return {
+        "title": info.get("title"),
+        "channel": info.get("uploader"),
+        "formats": formats[:10]
+    }
+
+@app.get("/download")
+def download(url: str):
+
+    options = {
+        "outtmpl": f"{DOWNLOAD_DIR}/%(title)s.%(ext)s",
+        "format": "best"
+    }
+
+    with YoutubeDL(options) as ydl:
+
+        info = ydl.extract_info(url, download=True)
+
+        filename = ydl.prepare_filename(info)
+
+    return {
+        "status": "downlaod complete",
+        "file": filename
+    }
